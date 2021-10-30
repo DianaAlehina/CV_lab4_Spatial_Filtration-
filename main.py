0a70620a665802ec2d14f4b516cee032cbe0923e
DianaAlehina <90766978+DianaAlehina@users.noreply.github.com>
2021-10-31 00:16:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,38 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-
-def showimg_first(img, title=None):
-    plt.imshow(img, cmap='gray')
-    if title is not None:
-        plt.title(title)
+def showimg_second(img1, img2, title1=None, title2=None):
+    plt.subplot(1, 2, 1)
+    plt.imshow(img1, cmap='gray')
     plt.axis('off')
+    if title1 is not None:
+        plt.title(title1)
+    plt.subplot(1, 2, 2)
+    plt.imshow(img2, cmap='gray')
+    plt.axis('off')
+    if title2 is not None:
+        plt.title(title2)
     plt.show()
 
 
-def showimg(img1, img2, img3, img4, title=None):
-    plt.subplot(1, 4, 1)
+def showimg(img1, img2, img3, img4, img5, img6, img7, img8):
+    plt.subplot(2, 4, 1)
     plt.imshow(img1, cmap='gray')
     plt.axis('off')
-    plt.title('')
-    plt.subplot(1, 4, 2)
+    plt.title('Original')
+    plt.subplot(2, 4, 2)
     plt.imshow(img2, cmap='gray')
     plt.axis('off')
-    plt.subplot(1, 4, 3)
+    plt.title('Laplacian')
+    plt.subplot(2, 4, 3)
     plt.imshow(img3, cmap='gray')
     plt.axis('off')
-    plt.title('')
-    plt.subplot(1, 4, 4)
+    plt.title('Original + Laplacian')
+    plt.subplot(2, 4, 4)
     plt.imshow(img4, cmap='gray')
     plt.axis('off')
-    if title is not None:
-        plt.title(title)
+    plt.title('Sobel')
+    plt.subplot(2, 4, 5)
+    plt.imshow(img5, cmap='gray')
+    plt.axis('off')
+    plt.title('Smoothing, 5x5')
+    plt.subplot(2, 4, 6)
+    plt.imshow(img6, cmap='gray')
+    plt.axis('off')
+    plt.title('Mask=(Original+Laplacian)*Smoothing')
+    plt.subplot(2, 4, 7)
+    plt.imshow(img7, cmap='gray')
+    plt.axis('off')
+    plt.title('Original + Mask')
+    plt.subplot(2, 4, 8)
+    plt.imshow(img8, cmap='gray')
+    plt.axis('off')
+    plt.title('Gamma correction')
     plt.show()
 
 
 def open_image(filename):
     try:
-        image = cv2.imread(filename, 0)
+        image = cv2.imread(filename)
         return image
     except FileNotFoundError:
         print("Файл не найден")
@@ -42,50 +63,66 @@ def open_image(filename):
 
 
 # нормализация(линейоное растяжение)
-def normalization(image_laplacian):
-    Imax = np.max(image_laplacian)
-    Imin = np.min(image_laplacian)
+def normalization(image):
+    Imax = np.max(image)
+    Imin = np.min(image)
     Omin, Omax = 0, 255
     a = float(Omax - Omin) / (Imax - Imin)
     b = Omin - a * Imin
-    image = a * image_laplacian + b
+    image = a * image + b
     image = image.astype(np.uint8)
     return image
 
 
 def image_enhancement(image_original):
+    image_original = np.uint8(image_original)
+
     # Б: Применение оператора лапласиана к оригинальному изображению для обнаружения краев.
     image_laplacian = cv2.Laplacian(image_original, cv2.CV_64F)
-    new_image_laplacian = normalization(image_laplacian)
+    # Используем нормализацию, чтобы картинка не выходила за отрезок [0, 255]
+    image_laplacian = normalization(image_laplacian)
+    image_laplacian = np.uint8(image_laplacian)
 
-    # В: Повышение резкости = image_original + new_image_laplacian
-    image_addition = cv2.addWeighted(image_original, 1, new_image_laplacian, 1, 0.0)
+    # В: Повышение резкости = image_original + image_laplacian
+    image_addition = cv2.addWeighted(image_original, 1, image_laplacian, 1, 0)
+    image_addition = np.uint8(image_addition)
+    image_addition = normalization(image_addition)
 
     # Г: Применение градиентного оператора Собела к оригинальному изображению
-    image_sobelx = cv2.Sobel(image_original, cv2.CV_64F, 1, 0, ksize=3)
-    image_sobely = cv2.Sobel(image_original, cv2.CV_64F, 0, 1, ksize=3)
+    image_sobelx = cv2.Sobel(image_original, cv2.CV_64F, 1, 0, 3)
+    image_sobely = cv2.Sobel(image_original, cv2.CV_64F, 0, 1, 3)
     image_sobelx = cv2.convertScaleAbs(image_sobelx)
     image_sobely = cv2.convertScaleAbs(image_sobely)
-    image_sobelxy = cv2.addWeighted(image_sobelx, 1, image_sobely, 1, 0)
+    image_sobelxy = cv2.add(image_sobelx, image_sobely)
+    image_sobelxy = np.uint8(image_sobelxy)
 
-    # Д:
-    img_mask = cv2.blur(image_sobelxy, (5, 5))
+    # Д: Сглаживание градиентного изображения image_sobelxy, сохранив края изображения, size = 5
+    image_medianBlur = cv2.medianBlur(image_sobelxy, 5)
+    image_medianBlur = np.uint8(image_medianBlur)
 
-    # E:
-    img_mask1 = cv2.bitwise_and(image_addition, img_mask)
+    # E: Изображение-маска = image_addition * image_medianBlur
+    image_mask = 255 * (image_addition / 255 * image_medianBlur / 255)
+    image_mask = np.uint8(image_mask)
 
-    # Ж:
-    img_mask2 = cv2.addWeighted(image_original, 1, img_mask1, 0.4, 1.0) #cv2.add(image_original, img_mask1)
-    # З:
+    # Ж: Повышение резкости = image_original + image_mask
+    image_addition2 = cv2.add(image_original, image_mask)
+    image_addition2 = np.uint8(image_addition2)
+
+    # З: Градационная коррекция по степенному закону
     gamma = 0.5
-    img_mask3 = np.array(255 * (img_mask2 / 255) ** gamma, dtype='uint8')
+    image_correction = 255 * (image_addition2 / 255) ** gamma
+    image_correction = np.uint8(image_correction)
 
-    showimg(image_original, new_image_laplacian, image_addition, image_sobelxy)
-    showimg(img_mask, img_mask1, img_mask2, img_mask3)
+    # Показ изображений
+    showimg_second(image_original, image_laplacian, 'Original', 'Laplacian')
+    showimg_second(image_addition, image_sobelxy, 'Original + Laplacian', 'Sobel')
+    showimg_second(image_medianBlur, image_mask, 'Smoothing, 5x5', 'Mask=(Original+Laplacian)*Smoothing')
+    showimg_second(image_addition2, image_correction, 'Original + Mask', 'Gamma correction')
+    showimg(image_original, image_laplacian, image_addition, image_sobelxy, image_medianBlur, image_mask,
+            image_addition2, image_correction)
 
 
 if __name__ == '__main__':
     filename = "skeleton.jpg"
     image_original = open_image(filename)
-    # showimg_first(image_original)
     image_enhancement(image_original)
